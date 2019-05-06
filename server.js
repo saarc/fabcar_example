5f0c1cacefd4da85ccdd796d37ee7e371462fbad
saarc <saarc99@naver.com>
2019-05-06 16:32:26 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,24 +5,33 @@ var bodyParser = require('body-parser');
 // Constants
 const PORT = 8080;
 const HOST = '10.0.2.15';
+
+
  // Hyperledger Bridge
+//var Fabric_Client = require('fabric-client');
+//var util = require('util');
+//var os = require('os');
+
+//
 const { FileSystemWallet, Gateway } = require('fabric-network');
 const fs = require('fs');
 const path = require('path');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
 const ccpPath = path.resolve(__dirname, '..', '..', 'basic-network', 'connection.json');
 const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
 const ccp = JSON.parse(ccpJSON);
 
-// 인터페이스 연동
+//Attach the middleware
 app.get('/', function (req, res) {
   fs.readFile('index.html', function (error, data) {
               res.send(data.toString());
 
   });
 });
+
 app.get('/api/query', async function (req, res) {
 		// create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
 
@@ -59,6 +68,7 @@ app.get('/api/query', async function (req, res) {
 //                        res.status(200).json({response: result.toString()});
 //			res.end(JSON.stringify(result));
 });
+// 192.168.1.56:8080/api/querycar?carno=CAR5
 app.get('/api/querycar/', async function (req, res) {
                 // create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
     try {
@@ -68,7 +78,33 @@ app.get('/api/querycar/', async function (req, res) {
 	var carno = req.query.carno;
 	console.log(carno);
 
-        result = contract_qeurycar(carno);
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const userExists = await wallet.exists('user1');
+        if (!userExists) {
+            console.log('An identity for the user "user1" does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: 'user1', discovery: { enabled: false } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('fabcar');
+
+        // Evaluate the specified transaction.
+        // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
+        // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
+        const result = await contract.evaluateTransaction('queryCar', carno);
 
         console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
         res.status(200).json({response: result.toString()});
@@ -77,19 +113,14 @@ app.get('/api/querycar/', async function (req, res) {
         process.exit(1);
     }
 });
+
 app.get('/api/createcar', function (req, res) {
   fs.readFile('createcar.html', function (error, data) {
               res.send(data.toString());
 
   });
 });
-app.get('/api/changeowner', function (req, res) {
-    fs.readFile('changeowner.html', function (error, data) {
-                res.send(data.toString());
-  
-    }); 
-});
-// 폼 인터페이스 처리
+
 app.post('/api/createcar/', async function (req, res) {
     try {
 	var carno = req.body.carno;
@@ -137,6 +168,14 @@ app.post('/api/createcar/', async function (req, res) {
     }   
 
 });
+
+app.get('/api/changeowner', function (req, res) {
+  fs.readFile('changeowner.html', function (error, data) {
+              res.send(data.toString());
+
+  }); 
+});
+
 app.post('/api/changeowner/', async function (req, res) {
     try {
         var carno = req.body.carno;
@@ -181,12 +220,9 @@ app.post('/api/changeowner/', async function (req, res) {
     }   
 
 });
-// 서버 가동
+
+
 app.listen(PORT, HOST);
 console.log(`Running on http://localhost:${PORT}`);
-// 컨트렉트 API
-
-
-
 
 
